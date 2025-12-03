c12e668f6aba3a5a53ce0d8bbb77bf5e4af44422
Fernando Yara <fyara@telesign.com>
2025-12-03 15:20:53 -0500

    change name

--- packages/boreal-web-components/stencil.config.ts
+++ packages/boreal-web-components/stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'boral-web-components',
+  namespace: 'boreal-web-components',
   outputTargets: [
     {
       type: 'dist',
