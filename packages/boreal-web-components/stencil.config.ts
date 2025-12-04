c9ff3a007eb3abcf5742b6c3295032623d6b03d4
Fernando Yara <fyara@telesign.com>
2025-12-04 12:09:04 -0500

    add output targets

--- packages/boreal-web-components/stencil.config.ts
+++ packages/boreal-web-components/stencil.config.ts
@@ -1,24 +1,35 @@
 import { Config } from '@stencil/core';
+import reactOutputTarget from './targets/react-output-target';
+import vueOutputTarget from './targets/vue-output-target';
 
 export const config: Config = {
   namespace: 'boreal-web-components',
+  minifyJs: true,
+  minifyCss: true,
+  sourceMap: false,
+  buildEs5: 'prod',
+  extras: {
+    experimentalSlotFixes: true,
+  },
   outputTargets: [
+    {
+      type: 'www',
+      serviceWorker: null,
+    },
     {
       type: 'dist',
       esmLoaderPath: '../loader',
+      dir: 'dist',
+      empty: true,
     },
     {
       type: 'dist-custom-elements',
-      customElementsExportBehavior: 'auto-define-custom-elements',
       externalRuntime: false,
+      dir: './components-build',
+      generateTypeDeclarations: true
     },
-    {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
-    },
+    reactOutputTarget(),
+    vueOutputTarget(),
   ],
   testing: {
     browserHeadless: "shell",
