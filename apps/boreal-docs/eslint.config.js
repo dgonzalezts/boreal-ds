584c2499202a5d42e19469967fc2c95a3a96a154
Daniel González <dgonzalez@telesign.com>
2026-01-08 15:03:30 -0500

    feat: EOA-8515 add eslint dependency and config

--- apps/boreal-docs/eslint.config.js
+++ apps/boreal-docs/eslint.config.js
@@ -1,9 +1,35 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import storybook from 'eslint-plugin-storybook';
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx}"],
+    plugins: {
+      js
+    },
+    extends: [
+      "js/recommended",
+    ],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    ignores: ['!.storybook'],
+  },
   tseslint.configs.recommended,
+  ...storybook.configs['flat/recommended'],
+  {
+    files: ["**/*.{ts,tsx}"],
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      'no-unused-vars': 'off',
+    },
+  },
 ]);
