9edcb69bdd65eb0f104b53ad6f85f62b974cce9b
Daniel González <dgonzalez@telesign.com>
2026-01-13 17:36:57 -0500

    feat: EOA-8515 add custom Storybook theme and logo

---
+++ apps/boreal-docs/.storybook/theme.ts
@@ -0,0 +1,8 @@
+import { create } from 'storybook/theming';
+
+export const customTheme = create({
+  base: 'light',
+  brandTitle: 'Boreal',
+  brandTarget: '_self',
+  brandImage: 'boreal-logo.png',
+});
