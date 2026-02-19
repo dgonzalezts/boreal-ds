1d45b57586489d63176044a55d3d03a34ab43732
Laura Cuellar <lcuellar@telesign.com>
2026-02-19 14:03:11 +0000

    feat: EOA-8969_typography_accessibility
    Merge in DEV/boreal-ds from feature/typography-accessibility to feature/typography
    
    Squashed commit of the following:
    
    commit f649f8b4a48ee9899adf2910bd6fb3c9ae048391
    Author: Laura Cuellar <laura.cuellar@masiv.com>
    Date:   Wed Feb 18 10:44:03 2026 -0500
    
        chore: updated import order
    
    commit 41ed689bbe2a99345e5d7b44f74e54376f200c18
    Author: Laura Cuellar <laura.cuellar@masiv.com>
    Date:   Wed Feb 18 10:38:53 2026 -0500
    
        chore: clean comments
    
    commit 1626d1e419588c776b02397b3918b47fee62642b
    Merge: 1b823a8 585dab5
    Author: Laura Cuellar <laura.cuellar@masiv.com>
    Date:   Wed Feb 18 10:34:53 2026 -0500
    
        chore: merge branch feature/typography into feature/typography-accessibility
    
    commit 1b823a8a4b3bb3b331d51943211f5be1be3d477f
    Author: Laura Cuellar <laura.cuellar@masiv.com>
    Date:   Wed Feb 18 09:51:55 2026 -0500
    
        chore: sync latest changes
    
    commit b9ad9593190e602c3da41ed9579e39e088aec0f7
    Author: Laura Cuellar <laura.cuellar@masiv.com>
    Date:   Mon Feb 16 10:37:25 2026 -0500
    
        feat(accessibility): created accessibility utility and added aria-tags
    
    commit 9aefb128920d70c5047354d4e1be3cf7f5ba4bbc
    Author: Laura Cuellar <laura.cuellar@masiv.com>
    Date:   Tue Feb 17 18:37:04 2026 -0500
    
        feat: updated prop names and types
    
    commit 15d9b7cbd01f926559ce2e15075dce6974cbd427
    Author: Laura Cuellar <laura.cuellar@masiv.com>
    Date:   Wed Feb 18 10:06:58 2026 -0500
    
        chore: accessibility updates
    
    commit 0d2002a8477c4c53bb4e91890e43a1ad002737ef
    Author: Laura Cuellar <laura.cuellar@masiv.com>
    Date:   Wed Feb 18 10:00:08 2026 -0500
    
        chore: review conflict
    
    commit 3e2f0e8824c6d50cbf51c4d568b26a9082eb0038
    Author: Laura Cuellar <laura.cuellar@masiv.com>
    Date:   Wed Feb 18 09:58:30 2026 -0500
    
        chore: remove duplicated
    
    commit 437593682ba8b978d22defaaf49e1d9e3ddbd4f6
    Author: Laura Cuellar <laura.cuellar@masiv.com>
    Date:   Wed Feb 18 09:51:55 2026 -0500
    
        chore: sync latest changes
    
    commit e93111ceda576bd2849962715993dc7cacc7dc85
    Author: Laura Cuellar <laura.cuellar@masiv.com>
    Date:   Mon Feb 16 10:37:25 2026 -0500
    
        feat(accessibility): created accessibility utility and added aria-tags

--- packages/boreal-web-components/src/components/bds-typography/bds-typography.tsx
+++ packages/boreal-web-components/src/components/bds-typography/bds-typography.tsx
@@ -1,24 +1,28 @@
-import { Component, Host, Prop, State, Element, h } from '@stencil/core';
-import type { ITypography } from './types/ITypography';
-import type { StyleModifiers } from '@/types/stylesMap';
+import { Component, Element, Host, Prop, State, Watch, h } from '@stencil/core';
 
-import { FILENAME, VARIANT_CONFIG, getAttributesByTag } from './utils/bds-typography-utils';
-import { TAG_ELEMENT, VARIANT_TYPOGRAPHY } from './types/enum';
+import type { StyleModifiers } from '@/types/stylesMap';
 import { ALIGNMENT } from '@/types/alignment';
 import { SIZES } from '@/types/size';
+import { STATES } from '@/types/states';
+import { Attributes, inheritAriaAttributes } from '@/utils/helpers/accessibility/a11y-attributes';
+
+import type { ITypography } from './types/ITypography';
+import { TAG_ELEMENT, VARIANT_TYPOGRAPHY } from './types/enum';
+import { FILENAME, getAttributesByTag, VARIANT_CONFIG } from './utils/bds-typography-utils';
 
 @Component({
   tag: 'bds-typography',
   styleUrl: 'bds-typography.scss',
 })
 export class BdsTypography implements ITypography {
+  private inheritedAttributes: Attributes = {};
+
   @Element() el!: HTMLBdsTypographyElement;
 
   @Prop({ reflect: true }) readonly variant: ITypography['variant'] = VARIANT_TYPOGRAPHY.DISPLAY;
   @Prop({ reflect: true }) readonly size: ITypography['size'] = SIZES.M;
   @Prop({ reflect: true }) readonly state: ITypography['state'];
 
-  // ? To apply utility classes or create an inherit attributes
   @Prop() readonly customClass: string;
 
   @Prop() readonly element: ITypography['element'] = TAG_ELEMENT.P;
@@ -26,6 +30,7 @@ export class BdsTypography implements ITypography {
   @Prop() readonly ellipsis: ITypography['ellipsis'] = false;
   @Prop() readonly maxLines: ITypography['maxLines'] = 1;
   @Prop() readonly tooltipText: ITypography['tooltipText'] = '';
+
   @Prop({ reflect: true }) readonly isRequired: ITypography['isRequired'] = false;
 
   @Prop() readonly htmlFor: ITypography['htmlFor'] = undefined;
@@ -37,21 +42,37 @@ export class BdsTypography implements ITypography {
 
   @State() sanitizedHref: string = '';
 
+  @Watch('href')
+  async updateSanitizedHref() {
+    if (this.getTagName === 'a' && this.href) {
+      const { sanitizeUrl } = await import('@braintree/sanitize-url');
+      this.sanitizedHref = sanitizeUrl(this.href);
+    } else this.sanitizedHref = '';
+  }
+
   async componentWillLoad() {
-    if (this.element === TAG_ELEMENT.A && this.href) await this.sanitizeLink();
+    if (this.getTagName === TAG_ELEMENT.A && this.href) await this.updateSanitizedHref();
+
+    this.inheritedAttributes = {
+      ...inheritAriaAttributes(this.el),
+    };
   }
 
   get getTagName() {
     return this.element.toLowerCase();
   }
 
-  private async sanitizeLink() {
-    const { sanitizeUrl } = await import('@braintree/sanitize-url');
-    this.sanitizedHref = sanitizeUrl(this.href);
-  }
-
   private getVariantConfig() {
     return VARIANT_CONFIG[this.variant] ?? {};
+  }
+
+  private getAccessibilityTags() {
+    const { isRequired = null } = this.getVariantConfig();
+    const isInteractiveTag = ['a', 'label'].includes(this.getTagName);
+    return {
+      'aria-required': this.isRequired && isRequired,
+      'aria-disabled': this.state === STATES.DISABLED && isInteractiveTag,
+    };
   }
 
   private getVariantStateStyles(): StyleModifiers {
@@ -63,7 +84,7 @@ export class BdsTypography implements ITypography {
       [`bds-typography--size-${this.size}`]: config?.size && config?.size.includes(this.size),
       [`bds-typography--required`]: config?.isRequired && this.isRequired,
       [`bds-typography--ellipsis`]: this.ellipsis && this.maxLines <= 1,
-      [`bds-typography--ellipsis-multiline`]: this.maxLines > 1,
+      [`bds-typography--ellipsis-multiline`]: this.ellipsis && this.maxLines > 1,
       [this.customClass]: !!this.customClass,
     };
   }
@@ -72,6 +93,8 @@ export class BdsTypography implements ITypography {
     const TagName = this.getTagName;
     const attributes = {
       ...getAttributesByTag(this, TagName),
+      ...this.inheritedAttributes,
+      ...this.getAccessibilityTags(),
     };
 
     const classes = this.getVariantStateStyles();
@@ -81,7 +104,11 @@ export class BdsTypography implements ITypography {
       <Host class="bds-typography">
         <TagName class={classes} style={{ webkitLineClamp: this.maxLines }} {...attributes}>
           <slot></slot>
-          {this.isRequired && isRequired && <em class="bds-typography__required-indicator">*</em>}
+          {this.isRequired && isRequired && (
+            <em class="bds-typography__required-indicator" aria-hidden="true">
+              *
+            </em>
+          )}
           {this.tooltipText && canUseTooltip && <em class="bds-typography__info-icon bds-icon-info-circle"></em>}
         </TagName>
       </Host>
