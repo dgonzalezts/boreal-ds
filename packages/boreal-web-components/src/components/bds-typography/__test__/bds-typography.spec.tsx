2f460f741224761e755e292c4f2dca325bbeb9df
Laura Cuellar <laura.cuellar@masiv.com>
2026-02-20 09:21:42 -0500

    test(web-components): * merge tests in one single file

--- packages/boreal-web-components/src/components/bds-typography/__test__/bds-typography.spec.tsx
+++ packages/boreal-web-components/src/components/bds-typography/__test__/bds-typography.spec.tsx
@@ -66,6 +66,223 @@ describe('bds-typography basic prop rendering', () => {
   });
 });
 
+describe('bds-typography variant dependant prop rendering', () => {
+  /**
+   * SIZE
+   */
+  it('should render with specified size when variant supports it', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography variant="display" size="l">Display text</bds-typography>',
+    });
+
+    const inner = root.firstElementChild;
+    expect(inner.classList.contains('bds-typography--size-l')).toBe(true);
+  });
+
+  it('should not render with specified size when variant does not support it', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography variant="label" size="m">Label text</bds-typography>',
+    });
+
+    const inner = root.firstElementChild;
+    expect(inner.classList.contains('bds-typography--size-m')).toBe(false);
+  });
+
+  it('should not render invalid size', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography variant="display" size="invalid-size">Label text</bds-typography>',
+    });
+
+    const inner = root.firstElementChild;
+    expect(inner.classList.contains('bds-typography--size-invalid-size')).toBe(false);
+  });
+
+  /**
+   * Tooltip icon
+   */
+  it('should render the tooltip icon when the variant supports it', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography element="label" variant="label" tooltip-text="This is a tooltip">Label text</bds-typography>',
+    });
+
+    const inner = getInner(root);
+    expect(inner.querySelector('.bds-typography__info-icon')).not.toBeNull();
+    expect(inner.querySelector('.bds-icon-info-circle')).not.toBeNull();
+  });
+
+  it('should not render the tooltip icon when variant does not support it', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography variant="link" tooltip-text="This is a tooltip">Link text</bds-typography>',
+    });
+
+    const inner = getInner(root);
+    expect(inner.querySelector('.bds-typography__info-icon')).toBeNull();
+    expect(inner.querySelector('.bds-icon-info-circle')).toBeNull();
+  });
+
+  it('should not render the tooltip icon when the tooltip text is empty', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography element="label" variant="label">Label text</bds-typography>',
+    });
+
+    const inner = getInner(root);
+    expect(inner.querySelector('.bds-typography__info-icon')).toBeNull();
+    expect(inner.querySelector('.bds-icon-info-circle')).toBeNull();
+  });
+
+  /**
+   * STATE
+   */
+  it('should render with specified state when variant supports it', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography variant="helper" state="error">This is an error helper text</bds-typography>',
+    });
+
+    const inner = root.firstElementChild;
+    expect(inner.classList.contains('bds-typography--error')).toBe(true);
+  });
+
+  it('should not render with specified state when variant does not support it', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography variant="heading" state="error">Heading text</bds-typography>',
+    });
+
+    const inner = root.firstElementChild;
+
+    expect(inner.classList.contains('bds-typography--error')).toBe(false);
+  });
+
+  it('should not render invalid state', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography variant="helper" state="invalid-state">Text</bds-typography>',
+    });
+
+    const inner = root.firstElementChild;
+    expect(inner.classList.contains('bds-typography--invalid-state')).toBe(false);
+  });
+
+  /**
+   * REQUIRED INDICATOR
+   */
+  it('should render the required indicator when variant supports it', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography element="label" variant="label" is-required="true">Label text</bds-typography>',
+    });
+
+    const inner = root.firstElementChild;
+    expect(inner.querySelector('.bds-typography__required-indicator')).not.toBeNull();
+  });
+
+  it('should not render with the required indicator when variant does not support it', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography variant="heading" is-required="true">Text</bds-typography>',
+    });
+
+    const inner = root.firstElementChild;
+    expect(inner.querySelector('.bds-typography__required-indicator')).toBeNull();
+  });
+
+  /**
+   * REQUIRED INDICATOR COLOR DEPENDANT ON STATE DISABLED
+   */
+  it('should render with required indicator and disabled when variant supports state disabled and required indicators', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography element="label" variant="label" is-required="true" state="disabled">Text disabled and required.</bds-typography>',
+    });
+
+    const inner = root.firstElementChild;
+    expect(inner.classList.contains('bds-typography--required')).toBe(true);
+    expect(inner.classList.contains('bds-typography--disabled')).toBe(true);
+  });
+});
+
+describe('bds-typography ellipsis functionality', () => {
+  it('should render with ellipsis in a single line', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography ellipsis="true">This is a long text with ellipsis.</bds-typography>',
+    });
+
+    const inner = root.firstElementChild;
+    expect(inner.classList.contains('bds-typography--ellipsis')).toBe(true);
+    expect(inner.classList.contains('bds-typography--ellipsis-multiline')).toBe(false);
+  });
+
+  it('should render with ellipsis when max lines > 1', async () => {
+    const lines = 2;
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: `<bds-typography ellipsis="true" max-lines="${lines}">This is a long text with ellipsis.</bds-typography>`,
+    });
+
+    const inner = getInner(root);
+
+    expect(inner.classList.contains('bds-typography--ellipsis')).toBe(false);
+    expect(inner.classList.contains('bds-typography--ellipsis-multiline')).toBe(true);
+    expect(inner.style.webkitLineClamp).toBe(`${lines}`);
+  });
+});
+
+describe('bds-typography link functionality', () => {
+  it('should render link variant with href', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography element="a" variant="link" href="#">Anchor text</bds-typography>',
+    });
+
+    const inner = getInner(root);
+
+    expect(inner).toBeTruthy();
+    expect(inner.getAttribute('href')).not.toBeNull();
+  });
+
+  it('should render link variant with target attribute', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography element="a" variant="link" href="#" target="_blank">Anchor text</bds-typography>',
+    });
+
+    const inner = getInner(root);
+
+    expect(inner.getAttribute('target')).toEqual('_blank');
+    expect(inner.getAttribute('rel')).toEqual('noopener noreferrer');
+  });
+
+  it('should render a downloadable link', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography element="a" variant="link" is-downloadable="true">Anchor text</bds-typography>',
+    });
+
+    const inner = getInner(root);
+
+    expect(inner.getAttribute('download')).toBeTruthy();
+  });
+
+  it('should render a downloadable link with custom filename', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography element="a" variant="link" is-downloadable="true" filename="custom_name.pdf">Anchor text</bds-typography>',
+    });
+
+    const inner = getInner(root);
+
+    expect(inner.getAttribute('download')).toEqual('custom_name.pdf');
+  });
+});
+
 describe('bds-typography dynamic tag attributes', () => {
   it('should set href, target and filename attributes only when element="a"', async () => {
     const { root } = await newSpecPage({
@@ -91,5 +308,29 @@ describe('bds-typography dynamic tag attributes', () => {
     expect(inner.getAttribute('href')).toBeNull();
     expect(inner.getAttribute('target')).toBeNull();
     expect(inner.getAttribute('download')).toBeNull();
+  });
+});
+
+describe('bds-typography accessibility attributes', () => {
+  it('should use htmlfor attribute when passing the prop htmlFor to a element="label"', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography element="label" variant="label" html-for="input-id-123">Hello world</bds-typography>',
+    });
+
+    const inner = getInner(root);
+
+    expect(inner.getAttribute('htmlfor')).not.toBeNull();
+  });
+
+  it('should use for attribute when element="label"', async () => {
+    const { root } = await newSpecPage({
+      components: [BdsTypography],
+      html: '<bds-typography element="label" variant="label" html-for="input-id-123">Hello world</bds-typography>',
+    });
+
+    const inner = getInner(root);
+
+    expect(inner.getAttribute('htmlfor')).not.toBeNull();
   });
 });
