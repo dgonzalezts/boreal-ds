9bde5056309934ab039aac38ad9f1d7e793227b8
Mateo Ap <mateo.aponte@telesign.com>
2026-03-03 18:24:37 -0500

    docs(web-components): EOA-10306 add Source component as code formatter and update settings.json

--- apps/boreal-docs/src/stories/foundations/typography/components/Helperstyle.tsx
+++ apps/boreal-docs/src/stories/foundations/typography/components/Helperstyle.tsx
@@ -1,24 +1,20 @@
 import { Card } from '@/components';
-import hljs from 'highlight.js';
+import { Source } from '@storybook/addon-docs/blocks';
 
 export const HelperStyle = ({ name, description }: any) => {
-  const content = (code: boolean = false) => {
-    if (code) {
-      return hljs.highlightAuto(
-        `<span class=${name}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>`
-      ).value;
-    }
-    return <span className={name}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>;
-  };
+  const JSXContent = (
+    <span className={name}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+  );
+  const stringContent = `<span class="${name}">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>`;
 
   return (
     <div className="helper-style-container">
       <h4>{name}</h4>
       {description && <p>{description}</p>}
       <Card variant="elevated" className="typography-preview__card">
-        {content()}
+        {JSXContent}
       </Card>
-      <code-block language="html" code={`${content(true)}`} />
+      <Source code={stringContent} language="html" dark />
     </div>
   );
 };
