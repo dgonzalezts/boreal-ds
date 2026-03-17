2989d70a8821f5613900706c6a5cb7a2dfb916e8
Daniel González <dgonzalez@telesign.com>
2026-03-17 17:26:59 -0500

    feat(web-components): EOA-10057 add ITextField interface and related enums

---
+++ packages/boreal-web-components/src/components/bds-text-field/types/enum.ts
@@ -0,0 +1,9 @@
+export const TEXT_FIELD_TYPES = {
+  TEXT: 'text',
+  PASSWORD: 'password',
+} as const;
+
+export const TEXT_FIELD_VARIANTS = {
+  OUTLINE: 'outline',
+  PLAIN: 'plain',
+} as const;
